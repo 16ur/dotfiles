eb899765ed1f90d9eb2c69e814a4d9471b8efe39
16ur <akuseruuu@protonmail.com>
2026-06-23 13:27:56 +0200

    feat: add keymaps for buffers and navigation

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -239,12 +239,14 @@ vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result centered" })
 vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result centered" })
 
 -- Files
+vim.keymap.set("n", "<leader>x", "<cmd>x<CR>", { desc = "Save and quit" })
 vim.keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Save file" })
 vim.keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "Quit window" })
 
 -- Buffers
 vim.keymap.set("n", "<leader>bn", "<cmd>bnext<CR>", { desc = "Next buffer" })
 vim.keymap.set("n", "<leader>bp", "<cmd>bprevious<CR>", { desc = "Previous buffer" })
+vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<CR>", { desc = "Delete buffer" })
 
 -- Splits
 vim.keymap.set("n", "<leader>sv", "<cmd>vsplit<CR>", { desc = "Split vertically" })
@@ -255,3 +257,13 @@ vim.keymap.set("n", "<leader>h", "<C-w>h", { desc = "Go to left window" })
 vim.keymap.set("n", "<leader>j", "<C-w>j", { desc = "Go to lower window" })
 vim.keymap.set("n", "<leader>k", "<C-w>k", { desc = "Go to upper window" })
 vim.keymap.set("n", "<leader>l", "<C-w>l", { desc = "Go to right window" })
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+
+
+vim.keymap.set("v", "<", "<gv", { desc = "Indent left and reselect" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
+
+
+})
